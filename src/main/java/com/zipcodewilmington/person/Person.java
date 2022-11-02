9f3836d4e56347cc5b96b203a05c88c2b4042df8
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-11-02 12:13:20 -0400

    Finished this lab: Set new fields, created new constructors, created new tests for fields and constructors

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,6 +7,16 @@ public class Person {
     private String name;
     private int age;
 
+    private int retirementAge;
+
+    private String address;
+
+    private String spouseName;
+
+    private String phoneNumber;
+
+    private String employeeID;
+
     public Person() {
         this("", Integer.MAX_VALUE);
     }
@@ -17,12 +27,51 @@ public class Person {
 
     public Person(String name) {
         this.name = name;
-
     }
 
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public Person(String name, int age, int retirementAge) {
+        this.name = name;
+        this.age = age;
+        this.retirementAge = retirementAge;
+    }
+
+    public Person(String name, int age, int retirementAge, String address) {
+        this.name = name;
+        this.age = age;
+        this.retirementAge = retirementAge;
+        this.address = address;
+    }
+
+    public Person(String name, int age, int retirementAge, String address, String spouseName) {
+        this.name = name;
+        this.age = age;
+        this.retirementAge = retirementAge;
+        this.address = address;
+        this.spouseName = spouseName;
+    }
+
+    public Person(String name, int age, int retirementAge, String address, String spouseName, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.retirementAge = retirementAge;
+        this.address = address;
+        this.spouseName = spouseName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Person(String name, int age, int retirementAge, String address, String spouseName, String phoneNumber, String employeeID) {
+        this.name = name;
+        this.age = age;
+        this.retirementAge = retirementAge;
+        this.address = address;
+        this.spouseName = spouseName;
+        this.phoneNumber = phoneNumber;
+        this.employeeID = employeeID;
     }
 
     public String setName(String name) {
@@ -39,5 +88,45 @@ public class Person {
 
     public Integer getAge() {
         return age;
+    }
+
+    public int getRetirementAge() {
+        return retirementAge;
+    }
+
+    public void setRetirementAge(int retirementAge) {
+        this.retirementAge = retirementAge;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSpouseName() {
+        return spouseName;
+    }
+
+    public void setSpouseName(String spouseName) {
+        this.spouseName = spouseName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -69,6 +69,138 @@ public class TestPerson {
     }
 
     @Test
+    public void testConstructorWithNameAndAgeAndRetirementAge() {
+        // Given
+        Integer expectedAge = 55;
+        String expectedName = "Leon";
+        Integer expectedRetirementAge = 70;
+
+        // When
+        Person person = new Person(expectedName, expectedAge, expectedRetirementAge);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+        Integer actualRetirementAge = person.getRetirementAge();
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedRetirementAge, actualRetirementAge);
+    }
+
+    @Test
+    public void testConstructorWithNameAndAgeAndRetirementAgeAndAddress() {
+        // Given
+        Integer expectedAge = 55;
+        String expectedName = "Leon";
+        Integer expectedRetirementAge = 70;
+        String expectedAddress = "11 Croydon Rd";
+
+        // When
+        Person person = new Person(expectedName, expectedAge, expectedRetirementAge, expectedAddress);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+        Integer actualRetirementAge = person.getRetirementAge();
+        String actualAddress = person.getAddress();
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedRetirementAge, actualRetirementAge);
+        Assert.assertEquals(expectedAddress, actualAddress);
+    }
+
+    @Test
+    public void testConstructorWithNameAndAgeAndRetirementAgeAndAddressAndSpouseName() {
+        // Given
+        Integer expectedAge = 55;
+        String expectedName = "Leon";
+        Integer expectedRetirementAge = 70;
+        String expectedAddress = "11 Croydon Rd";
+        String expectedSpouseName = "Mary";
+
+        // When
+        Person person = new Person(expectedName, expectedAge, expectedRetirementAge, expectedAddress, expectedSpouseName);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+        Integer actualRetirementAge = person.getRetirementAge();
+        String actualAddress = person.getAddress();
+        String actualSpouseName = person.getSpouseName();
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedRetirementAge, actualRetirementAge);
+        Assert.assertEquals(expectedAddress, actualAddress);
+        Assert.assertEquals(expectedSpouseName, actualSpouseName);
+    }
+
+    @Test
+    public void testConstructorWithNameAndAgeAndRetirementAgeAndAddressAndSpouseNameAndPhoneNumeber() {
+        // Given
+        Integer expectedAge = 55;
+        String expectedName = "Leon";
+        Integer expectedRetirementAge = 70;
+        String expectedAddress = "11 Croydon Rd";
+        String expectedSpouseName = "Mary";
+        String expectedPhoneNumber = "3025555555";
+
+        // When
+        Person person = new Person(expectedName, expectedAge, expectedRetirementAge, expectedAddress, expectedSpouseName, expectedPhoneNumber);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+        Integer actualRetirementAge = person.getRetirementAge();
+        String actualAddress = person.getAddress();
+        String actualSpouseName = person.getSpouseName();
+        String actualPhoneNumber = person.getPhoneNumber();
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedRetirementAge, actualRetirementAge);
+        Assert.assertEquals(expectedAddress, actualAddress);
+        Assert.assertEquals(expectedSpouseName, actualSpouseName);
+        Assert.assertEquals(expectedPhoneNumber, actualPhoneNumber);
+
+    }
+
+    @Test
+    public void testConstructorWithNameAndAgeAndRetirementAgeAndAddressAndSpouseNameAndPhoneNumeberAndEmployeeID() {
+        // Given
+        Integer expectedAge = 55;
+        String expectedName = "Leon";
+        Integer expectedRetirementAge = 70;
+        String expectedAddress = "11 Croydon Rd";
+        String expectedSpouseName = "Mary";
+        String expectedPhoneNumber = "3025555555";
+        String expectedEmployeeID = "123456789";
+
+
+        // When
+        Person person = new Person(expectedName, expectedAge, expectedRetirementAge, expectedAddress, expectedSpouseName, expectedPhoneNumber, expectedEmployeeID);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+        Integer actualRetirementAge = person.getRetirementAge();
+        String actualAddress = person.getAddress();
+        String actualSpouseName = person.getSpouseName();
+        String actualPhoneNumber = person.getPhoneNumber();
+        String actualEmployeeID = person.getEmployeeID();
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedRetirementAge, actualRetirementAge);
+        Assert.assertEquals(expectedAddress, actualAddress);
+        Assert.assertEquals(expectedSpouseName, actualSpouseName);
+        Assert.assertEquals(expectedPhoneNumber, actualPhoneNumber);
+        Assert.assertEquals(expectedEmployeeID, actualEmployeeID);
+    }
+
+    @Test
     public void testSetName() {
         // Given
         Person person = new Person();
@@ -93,6 +225,73 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetRetirementAge() {
+        // Given
+        Person person = new Person();
+        Integer expected = 70;
+
+        // When
+        person.setRetirementAge(expected);
+
+        // Then
+        Integer actual = person.getRetirementAge();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetAddress() {
+        // Given
+        Person person = new Person();
+        String expected = "11 Croydon";
+
+        // When
+        person.setAddress(expected);
+
+        // Then
+        String actual = person.getAddress();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetSpouseName() {
+        // Given
+        Person person = new Person();
+        String expected = "Mary";
+
+        // When
+        person.setSpouseName(expected);
+
+        // Then
+        String actual = person.getSpouseName();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetPhoneNumber() {
+        // Given
+        Person person = new Person();
+        String expected = "3025555555";
+
+        // When
+        person.setPhoneNumber(expected);
+
+        // Then
+        String actual = person.getPhoneNumber();
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testSetEmployeeID() {
+        // Given
+        Person person = new Person();
+        String expected = "123456789";
+
+        // When
+        person.setEmployeeID(expected);
+
+        // Then
+        String actual = person.getEmployeeID();
         Assert.assertEquals(expected, actual);
     }
 }
